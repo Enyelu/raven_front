6f60125134ccf981f5ce20ae32a6843f82aa874a
rabo <rabobahago@gmail.com>
2021-12-28 22:09:43 +0100

    login page input and image tags added

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,4 +1,39 @@
+/* eslint-disable jsx-a11y/alt-text */
+import "./login.scss";
+import profile from "../images/a.png";
+import email from "../images/email.jpg";
+import pass from "../images/pass.png";
 const Login = () => {
-  return <div className="login">Input</div>;
+  return (
+    <div className="login-main">
+      <div className="sub-login">
+        <div>
+          <div className="login-img">
+            <div className="container-img">
+              <img />
+            </div>
+          </div>
+          <div>
+            <h1>Login Page</h1>
+            <div>
+              <img src={email} alt="email" className="email" />
+              <input type="text" placeholder="user name" className="name" />
+            </div>
+            <div className="second-input">
+              <img src={pass} alt="pass" className="email" />
+              <input type="password" placeholder="user name" className="name" />
+            </div>
+            <div className="login-button">
+              <button>Login</button>
+            </div>
+
+            <p className="link">
+              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Login;
