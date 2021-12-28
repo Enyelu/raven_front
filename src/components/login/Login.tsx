383d1fb0cea4a5bec711c9eac0cf4e9ca7719721
rabo <rabobahago@gmail.com>
2021-12-28 22:57:46 +0100

    login component scss added

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable jsx-a11y/alt-text */
-import "./login.scss";
+import { NavLink } from "react-router-dom";
 import profile from "../images/a.png";
 import email from "../images/email.jpg";
 import pass from "../images/pass.png";
+import "./login.scss";
 const Login = () => {
   return (
     <div className="login-main">
@@ -10,25 +11,32 @@ const Login = () => {
         <div>
           <div className="login-img">
             <div className="container-img">
-              <img />
+              <img src={profile} alt="profile" className="profile" />
             </div>
           </div>
           <div>
-            <h1>Login Page</h1>
-            <div>
-              <img src={email} alt="email" className="email" />
-              <input type="text" placeholder="user name" className="name" />
-            </div>
-            <div className="second-input">
-              <img src={pass} alt="pass" className="email" />
-              <input type="password" placeholder="user name" className="name" />
-            </div>
-            <div className="login-button">
-              <button>Login</button>
-            </div>
-
-            <p className="link">
-              <a href="#">Forgot password ?</a> Or<a href="#">Sign Up</a>
+            <h1>Login</h1>
+            <form>
+              <div className="first-class">
+                <img src={email} alt="email" className="email" />
+                <input type="text" placeholder="user name" className="name" />
+              </div>
+              <div className="second-input">
+                <img src={pass} alt="pass" className="email" />
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="name"
+                />
+              </div>
+              <div className="login-button">
+                <button className="inner-button">Login</button>
+              </div>
+            </form>
+            <p className="login-link">
+              <NavLink to="#" className="NavLink">
+                Forgot Password?
+              </NavLink>
             </p>
           </div>
         </div>
