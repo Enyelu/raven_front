34385fbe1f95759a2ca61ed7f0e2e5b41af11bba
rabo <rabobahago@gmail.com>
2022-01-02 22:10:35 +0100

    react icons added to the profile component

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,39 +1,65 @@
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as FcIcons from "react-icons/fc";
 import "./profile.scss";
 const Profile = () => {
   return (
     <div className="profile-container">
       <div className="profile-1 profile-item">
         <div className="profile-status">
-          <span>Status:Active</span>
+          <h4>Jone Anas</h4>
+          <span>{<FaIcons.FaUserCheck />} Status:Active</span>
           <img src="../images/pass1.jpg" alt="" />
-          <p>224-667-4566</p>
-          <p>23-456-678</p>
-          <p>+469900</p>
-          <div>
-            <span>twitter</span>
-            <span>facebook</span>
-            <span>linkedin</span>
-            <span>teams</span>
-          </div>
+          <p>{<FaIcons.FaPhoneAlt />} 224-667-4566</p>
+          <p>{<FaIcons.FaShuttleVan />} 23-456-678</p>
+          <p>{<FaIcons.FaMoneyBillWaveAlt />} 9900</p>
+          <ul className="profile-icons">
+            <li>
+              <Link to="">
+                <FaIcons.FaFacebookF />
+              </Link>
+            </li>
+            <li>
+              <Link to="">
+                <FaIcons.FaLinkedinIn />
+              </Link>
+            </li>
+            <li>
+              <Link to="">
+                <FaIcons.FaTwitter />
+              </Link>
+            </li>
+            <li>
+              <Link to="">
+                <FaIcons.FaMediumM />
+              </Link>
+            </li>
+          </ul>
         </div>
         <hr className="horizontal" />
         <div className="profile-info">
-          <h5>Hire Information</h5>
+          <h5>{<FcIcons.FcConferenceCall />}Hire Information</h5>
           <p>
-            Started on:<span>12/03/2020</span>
+            Started on:<span>02/01/2020</span>
           </p>
           <p>
-            Worked here for:<span>10years</span>{" "}
+            Worked here for:<span>2 years</span>{" "}
           </p>
         </div>
         <hr className="horizontal" />
         <div className="profile-occupation">
-          <span>IT</span>
-          <span>Full-Time</span>
-          <span>Lagos, Nigeria</span>
+          <span>{<FaIcons.FaUserCog />} IT</span>
+          <span>{<FcIcons.FcClock />} Full-Time</span>
+          <span>{<FcIcons.FcHome />} Lagos, Nigeria</span>
         </div>
       </div>
-      <div className="profile-2 profile-item">Profile</div>
+      <div className="profile-2 profile-item">
+        <div className="profile-personal">
+          <h2>personal</h2>
+        </div>
+        <hr />
+        <div></div>
+      </div>
     </div>
   );
 };
