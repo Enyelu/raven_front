e27968cd07a578d04c0f9f42bde2ee613ac31748
Chukwuebuka Joseph Enyelu <careers.joseph@yahoo.com>
2022-01-02 23:25:55 +0100

    resolving conflict

--- src/components/slackOnboard/SlackOnboard.jsx
+++ src/components/slackOnboard/SlackOnboard.jsx
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-//import React from 'react'
-import "./SlackOnboard.scss"
-import SlackImage from "../images/slack.png"
-=======
 /* eslint-disable jsx-a11y/img-redundant-alt */
 // eslint-disable-next-line jsx-a11y/img-redundant-alt
 import "./SlackOnboard.scss";
 import SlackImage from "../images/slack.png";
->>>>>>> 34385fbe1f95759a2ca61ed7f0e2e5b41af11bba
 
 import React, {useEffect, useState} from 'react'
 import agent from "../../app/api/Agent"
@@ -17,50 +11,6 @@ import agent from "../../app/api/Agent"
 
 
 export default function SlackOnboard() {
-<<<<<<< HEAD
-
-    const [employeeList, setEmployeeList] = useState();
-    const people = [{firstName: 'Mike'}, {firstName: 'Kelechi'}]
-
-    localStorage.setItem('people', JSON.stringify(people))
-    const value = JSON.parse(localStorage.getItem('people'))
-    console.log(value);
-
-    useEffect(async () => {
-        const Employees = await agent.RavenAccess.getAllEmployees(1,1);
-
-        if(Employees !== null)
-        {
-            console.log(Employees);
-            console.log(Employees.data.pageItems);
-
-            setEmployeeList(Employees.data.pageItems);
-        }
-        
-    }, [])
-    
-    localStorage.setItem('EmployeesList', JSON.stringify(employeeList));
-    const List = localStorage.getItem('EmployeesList');
-    console.log(List);
-
-    return (
-        <div className='slack-main-div'>
-            <div className='slack-image-div'>
-                <img src={SlackImage} className='slack-image' alt='registration image'/>
-            </div>
-            <form className ="slack-form">
-                <h3 className='slack-block-title'>Onboard Slack</h3>
-                <div className='slack-input-div'>
-                    <input className='slack-input' type='text' placeholder='Employee Email'  required />
-                </div>
-                <button className='slack-button' type='submit'>Invite</button>
-            </form>  
-
-            <div >
-             {value.map((person) => (<h1 key={person.firstName}>{person.firstName}</h1>))}
-            </div>
-         
-=======
   return (
     <div className="slack-main-div">
       <div className="slack-image-div">
@@ -79,7 +29,6 @@ export default function SlackOnboard() {
             placeholder="Employee Email"
             required
           />
->>>>>>> 34385fbe1f95759a2ca61ed7f0e2e5b41af11bba
         </div>
         <button className="slack-button" type="submit">
           Invite
