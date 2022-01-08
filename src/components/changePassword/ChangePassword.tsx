e2bb19a10d4c6452adfc05e042452c5de3065185
rabo <rabobahago@gmail.com>
2022-01-08 15:49:05 +0100

    change password scss and basic form added

---
+++ src/components/changePassword/ChangePassword.tsx
@@ -0,0 +1,9 @@
+import "./ChangePassword.scss";
+const ChangePassword = () => {
+  return (
+    <div className="form-floating change-password">
+      <form></form>
+    </div>
+  );
+};
+export default ChangePassword;
