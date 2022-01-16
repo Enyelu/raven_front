dd2c36bc5c6bc18d52ba9e251f7052fb07e78ece
Chukwuebuka Joseph Enyelu <careers.joseph@yahoo.com>
2022-01-16 15:16:41 +0100

    modified agent component to handle api errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.scss";
 import Login from "./components/login/Login";
+import error404 from "./components/shared/error/error404"
 
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
       {/* <Header /> */}
       <Switch>
         <Route path="/login" component={Login} />
+        <Route path="/not-found" component={error404} />
+
+        <Redirect to="/not-found" />
         <Redirect to="/login" />
       </Switch>
     </div>
